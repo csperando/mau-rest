e30243b09e741386751a0e08034b26c6d2642118
csperando <csperando@gmail.com>
2022-08-05 18:26:16 -0400

    require header x-client-hostname to allow cors

--- cors.js
+++ cors.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const allowList = ["localhost:5000", "localhost:3000", "csperando.github.io"];
 
 function cors(req, res, next) {
-    let h = ("host" in req.headers) ? req.headers.host : "Error: no hostname";
+    // console.log(req.headers);
+    let h = ("x-client-host" in req.headers) ? req.get("x-client-host") : "Error: no hostname";
 
     if(allowList.indexOf(h) != -1) {
         console.log(`Request from [${h}] allowed.`);
@@ -12,7 +13,6 @@ function cors(req, res, next) {
 
     } else {
         console.log(`Error: hostname [${h}] is not allowed. Accepted hosts are ${JSON.stringify(allowList)}`);
-        res.setHeader("Access-Control-Allow-Origin", "*");
 
     }
 
