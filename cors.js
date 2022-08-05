b57244a93c87e2024e97b69c80a5d198754eedcc
csperando <csperando@gmail.com>
2022-08-05 17:48:56 -0400

    log errors

--- cors.js
+++ cors.js
@@ -6,11 +6,11 @@ const allowList = ["localhost", "csperando.github.io"];
 function cors(req, res, next) {
     let h = req.hostname;
     if(allowList.indexOf(h) != -1) {
-        // console.log(`Request from [${h}] allowed.`);
+        console.log(`Request from [${h}] allowed.`);
         res.setHeader("Access-Control-Allow-Origin", "https://" + h);
 
     } else {
-        // console.log(`Error: hostname [${h}] is not allowed. Accepted hosts are ${JSON.stringify(allowList)}`);
+        console.log(`Error: hostname [${h}] is not allowed. Accepted hosts are ${JSON.stringify(allowList)}`);
 
     }
 
