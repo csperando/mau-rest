c8ddd81d6cdcd92428ab2ed0bb43df030c6ba635
csperando <csperando@gmail.com>
2022-08-05 18:38:36 -0400

    try using origin header for cors

--- cors.js
+++ cors.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const allowList = ["localhost:5000", "localhost:3000", "csperando.github.io"];
+const allowList = ["http://localhost:5000", "http://localhost:3000", "https://csperando.github.io"];
 
 function cors(req, res, next) {
     console.log(req.headers);
-    let h = ("x-client-host" in req.headers) ? req.get("x-client-host") : "Error: no hostname";
+    let h = ("origin" in req.headers) ? req.get("origin") : "Error: no hostname";
 
     if(allowList.indexOf(h) != -1) {
         console.log(`Request from [${h}] allowed.`);
-        res.setHeader("Access-Control-Allow-Origin", "https://" + h);
+        res.setHeader("Access-Control-Allow-Origin", h);
 
     } else {
         console.log(`Error: hostname [${h}] is not allowed. Accepted hosts are ${JSON.stringify(allowList)}`);
