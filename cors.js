bd033440bec7f83c8d0e8dc948baaa0b598cf21f
csperando <csperando@gmail.com>
2022-08-05 18:13:01 -0400

    try getting hostname from req.headers. if none exist or not whitelisted, then '*'

--- cors.js
+++ cors.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const allowList = ["localhost", "csperando.github.io"];
+const allowList = ["localhost:5000", "localhost:3000", "csperando.github.io"];
 
 function cors(req, res, next) {
-    let h = req.hostname;
+    let h = ("host" in req.headers) ? req.headers.host : "Error: no hostname";
+
     if(allowList.indexOf(h) != -1) {
         console.log(`Request from [${h}] allowed.`);
         res.setHeader("Access-Control-Allow-Origin", "https://" + h);
 
     } else {
         console.log(`Error: hostname [${h}] is not allowed. Accepted hosts are ${JSON.stringify(allowList)}`);
+        res.setHeader("Access-Control-Allow-Origin", "*");
 
     }
 
