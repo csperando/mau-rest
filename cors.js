abaad848eafcc96314084baf50eec9ea9bafd0ea
csperando <csperando@gmail.com>
2022-08-05 21:36:02 -0400

    test

--- cors.js
+++ cors.js
@@ -4,12 +4,17 @@ const bodyParser = require("body-parser");
 const allowList = ["http://localhost:5000", "http://localhost:3000", "https://csperando.github.io"];
 
 function cors(req, res, next) {
-    // console.log(req.headers);
+    console.log(req.headers);
     let h = ("origin" in req.headers) ? req.get("origin") : "Error: no hostname";
+    let r = ("referrer" in req.headers) ? req.get("referrer") : "Error: no referrer";
 
     if(allowList.indexOf(h) != -1) {
         // console.log(`Request from [${h}] allowed.`);
         res.setHeader("Access-Control-Allow-Origin", h);
+        res.setHeader("origin", "vary");
+
+    } else if(allowList.indexOf(r) != -1) {
+        res.setHeader("Access-Control-Allow-Origin", r);
         res.setHeader("origin", "vary");
 
     } else {
